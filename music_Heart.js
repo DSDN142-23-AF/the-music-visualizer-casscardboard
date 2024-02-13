e569e81d08b3f7c217c70c3108cf421175f576dd
casscardboard <128674467+casscardboard@users.noreply.github.com>
2024-02-13 15:01:55 +1300

    updates
    updates

---
+++ music_Heart.js
@@ -0,0 +1,34 @@
+
+// vocal, drum, bass, and other are volumes ranging from 0 to 100
+let firstRun = true;
+let heartImg;
+let bodyImg;
+load_image("body, png")
+load_image("heart, png")
+
+
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  if(firstRun){
+ firstRun = false
+   
+  }
+  background(100)
+  push()
+scale(0.4)
+image(heart.png, 0,drum)
+pop()
+}
+ 
+
+function draw_one_frame(words, vocal, drum, bass, other, counter) {
+  if(firstRun){
+ firstRun = false
+background(100)
+push()
+scale(100)
+image(body.png), 0,bass)
+pop()
+
+
+
+}   
