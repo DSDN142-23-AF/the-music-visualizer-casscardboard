2a60c6a276165ec513f60f4d97d1c24f4ad863b6
casscardboard <128674467+casscardboard@users.noreply.github.com>
2024-02-16 13:22:35 +1300

    final updates
    final updates

--- music_Heart.js
+++ music_Heart.js
@@ -5,6 +5,8 @@ let heartImg;
 let bodyImg;
 let wingsImg;
 let wings2Img;
+let nameImg;
+let fairyImg;
 
 
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
@@ -13,125 +15,79 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
     bodyImg = loadImage('body.png')
     wingsImg = loadImage('wings.png')
     wings2Img = loadImage('wings 2 .png')
+    nameImg = loadImage('name.png')
+    fairyImg = loadImage('fairy.png')
 
  firstRun = false
    
   }
   background(100)
   push()
-scale(0.4)
+scale(0.4)// BACKROUND BODY 
 image(bodyImg, 0,0)
 pop()
 
 push()
-scale(0.8)
+scale(0.8)// HEART MOM 
 image(heartImg, -740,drum-450) // name of image, xpos, Ypos 
 pop()
 
 push()
+scale(0.3)// cassidy
+image(nameImg, drum-80 , 5000) // name of image, xpos, Ypos 
+pop()
+
+push()
+scale(0.1)// lesbian fairies 1
+image(fairyImg, bass+3600, 13000) // name of image, xpos, Ypos 
+pop()
+
+push()
+scale(0.1)// lesbian fairies 2
+image(fairyImg, other-500, 13000) // name of image, xpos, Ypos 
+pop()
+
+push()
+scale(0.2)// WINGS 2 
+image(wings2Img, words-700 , 6000)
+pop()
+
+push() // WINGS 1
 scale(0.2)
-image(wings2Img, drum-700 , 6000)
+image(wingsImg, words+1300, 6000)
 pop()
 
-push()
-scale(0.8)
-image(heartImg, -740,drum-450) // name of image, xpos, Ypos 
-pop()
-
-push()
-scale(0.2)
-image(wingsImg, drum+1200, 6000)
-pop()
-
-
-
-
-
-
-
-push()
-
+push()// FIRST BLOOD DRP 
 var ballSize = map(bass, 0,100, 40,150);
-
-var drumHight = map(drum, 0,100, 0+ballSize/2, height );
-
 stroke(0)
 fill (128,0,0);
-
 beginShape();
 scale(1)
 fill (128,0,0)
-
 ellipse(width/3.4,drum+600, ballSize-60);
 
-//curveVertex(500,250)
-//curveVertex(520,160)
-//curveVertex(620,170)
-//curveVertex(720,180)
-//curveVertex(820,190)
-
-
-//endShape(CLOSE);
-
-
-//ellipse(width/2, drum, ballSize);
-
 pop()
-
-push()
-var ballSize = map(bass, 0,100, 40,150);
-
-
-
+push() //SECOND BLOOD DRIP
+var ballSize = map(other, 0,100, 40,150);
 stroke(0)
 fill (128,0,0);
-
 beginShape();
 scale(1)
 fill (128,0,0)
-
 ellipse(width/3.4,drum+800,ballSize-60);
-
-
-
 pop()
 
 
-
-push()
+push()// THIRD BLOOD DRIP
 var ballSize = map(bass, 0,100, 40,150);
-
-
-
 stroke(0)
 fill (128,0,0);
-
 beginShape();
 scale(1)
 fill (128,0,0)
-
-ellipse(width/3.4,drum+1000,ballSize-70);
-
+ellipse(width/3.4,vocal+1000,ballSize-70);
+pop()
 
 
 
 }
-
-
-
-
-
- 
-
-// function draw_one_frame(words, vocal, drum, bass, other, counter) {
-//   if(firstRun){
-//  firstRun = false
-// background(100)
-// push()
-// scale()
-// image(body.png), 0,bass)
-// pop()
-
-
-
-// }   
